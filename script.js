daf5d90097810fdc375837e4d534c9e97c86e1d2
bbantier <65014410+bbantier@users.noreply.github.com>
2025-03-31 14:43:24 +0200

    fix: add export and import statements
    Using ES Modules syntax to export from script.js and import in
    script.test.js allows jest to run its tests on the capitalize function.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const capitalize = (string) => {
   return string.at(0).toUpperCase() + string.substring(1);
-}
+};
+
+export { capitalize };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-
+import { capitalize } from "./script.js";
 
 test("Capitalized 'camembert' should be 'Camembert'", () => {
   expect(capitalize("camembert")).toBe("Camembert");
